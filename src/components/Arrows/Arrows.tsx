f7215880826d194e187f272178122851a93462cc
Danya_Vid <danilviduykov@gmail.com>
2022-08-28 09:12:56 +0500

    add arrows

--- src/components/Arrows/Arrows.tsx
+++ src/components/Arrows/Arrows.tsx
@@ -1,3 +1,5 @@
+import { faArrowDown, faArrowLeft, faArrowRight, faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FC, useEffect, useState } from "react";
 import { useTypedSelector } from "../../hooks/useTypedSelector";
 import styles from "./Arrows.module.scss";
@@ -6,16 +8,25 @@ interface ArrowsProps {
     className?: string;
 }
 
+const paths: any = {
+    '-3': <FontAwesomeIcon className={styles.arrows__icon} icon={faArrowUp} />,
+    '-1': <FontAwesomeIcon className={styles.arrows__icon} icon={faArrowLeft} />,
+    '1': <FontAwesomeIcon className={styles.arrows__icon} icon={faArrowRight} />,
+    '3': <FontAwesomeIcon className={styles.arrows__icon} icon={faArrowDown} />,
+}
+
 const Arrows: FC<ArrowsProps> = ({className}) => {
     const uuid = require('uuid')
     const { directions, game } = useTypedSelector(state => state.appReducer);
     const [ arrows, setArrows ] = useState<number[]>([]);
-    const [ count, setCount ] = useState(0)
+    const [ count, setCount ] = useState(0);
 
     useEffect(() => {
         if (game) {
             const interval = setInterval(() => {
-                setArrows(state => [...state, directions[count]]);
+                const arrow = directions[count].toString();
+
+                setArrows(state => [...state, paths[arrow]]);
                 setCount(count + 1);
             }, 500);
          
